7db2ed25a2aac401fb9d958a6f38df50ad934d68
GhosTheKaos3150 <uriel_anderson@hotmail.com>
2023-11-16 15:51:52 -0300

    Vision OK

---
+++ google_cloud/vision.py
@@ -0,0 +1,36 @@
+import io
+
+from os import environ
+from google.cloud import vision
+from google.oauth2 import service_account
+
+from PIL import Image, ImageDraw, ImageFont
+
+async def get_vision_annotated(file):
+    credentials = service_account.Credentials.from_service_account_file("cloudbot-405122-70829b67fa85.json")
+    
+    client = vision.ImageAnnotatorClient(credentials=credentials)
+    image = vision.Image()
+    
+    image.content = bytes(file)
+    
+    response = client.object_localization(image=image).localized_object_annotations
+    
+    print(response)
+    
+    font = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\arial.ttf', 20)
+    img = Image.open(io.BytesIO(bytes(file)))
+    draw = ImageDraw.Draw(img)
+    
+    for object_ in response:
+        ul_vt = object_.bounding_poly.normalized_vertices[0]
+        dr_vt = object_.bounding_poly.normalized_vertices[2]
+        
+        text_vt = ((ul_vt.x*img.width)+1, (ul_vt.y*img.height))
+        ul_vt = (ul_vt.x*img.width, ul_vt.y*img.height)
+        dr_vt = (dr_vt.x*img.width, dr_vt.y*img.height)
+        
+        draw.rectangle([ul_vt, dr_vt], outline="red")
+        draw.text(text_vt, object_.name, fill="red", font=font)
+    
+    img.save("generated/vision.png", "PNG")
--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,7 +1,11 @@
 from os import environ
 
+from google_cloud import vision
+
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, ConversationHandler, filters
+
+ID_RESULT = range(2)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -16,6 +20,48 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         text=f"BOT: {msg}"
         )
+    
+async def identify_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Envie uma imagem a ser analisada!"
+        )
+    
+    return ID_RESULT
+
+async def identify_result(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    file = await update.message.photo[-1].get_file()
+    file_bytes = await file.download_as_bytearray()
+    
+    await vision.get_vision_annotated(file_bytes)
+    
+    await context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open("generated/vision.png", "rb")
+    )
+    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Aqui está a imagem analisada!"
+        )
+    
+    return ConversationHandler.END
+
+async def identify_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Operação Cancelada"
+        )
+    
+    return ConversationHandler.END
+
+async def identify_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Por favor, envie uma imagem ou digite /cancel para cancelar a operação!"
+        )
+    
+    return ID_RESULT
 
 async def not_found(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -29,12 +75,26 @@ def main():
     start_handler = CommandHandler('start', start)
     echo_handler = CommandHandler('echo', echo)
     
+    # Conversation
+    image_handler = ConversationHandler(
+        entry_points=[CommandHandler('identify', identify_start)],
+        states={
+            ID_RESULT: [
+                MessageHandler(filters.PHOTO, identify_result), 
+                CommandHandler('cancel', identify_cancel),
+                MessageHandler(filters.TEXT, identify_error)
+                ]
+        },
+        fallbacks=[CommandHandler('cancel', identify_cancel)]
+        )
+    
     # 404
     not_found_handler = MessageHandler(filters.COMMAND, not_found)
     
     # Adicionar handlers
     app.add_handler(start_handler)
     app.add_handler(echo_handler)
+    app.add_handler(image_handler)
     app.add_handler(not_found_handler)
     
     app.run_polling()
